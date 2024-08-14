45be3f52746a82a1190feb34f9ca35e165e5770e
VictorLapaDev <victorlucaslapa@gmail.com>
2024-08-13 22:08:42 -0300

    Feat: rotas do react native

---
+++ src/pages/HomeScreen.js
@@ -0,0 +1,18 @@
+import { Button, Text, View } from "react-native"
+
+export default function HomeScreen({navigation}){
+    return(
+        <View>
+            <Text>Home Screen</Text>
+            <Button
+                    title="Go to Details"
+                    onPress={() => navigation.navigate("Details")}
+            />
+            <Button
+                    title="Go to Profile"
+                    onPress={() => navigation.navigate("Profile")}
+            />
+
+        </View>
+    )
+}
